dd2765a91e1ad01a7d604ca696e7c70dffa45f89
juan123sebastian <122684235+juan123sebastian@users.noreply.github.com>
2023-01-17 17:35:31 +0000

    Subo algoritmo para ofertas

--- Tienda/index.js
+++ Tienda/index.js
@@ -1,3 +1,7 @@
+let caninos=0
+let felinos=0
+let aves=0
+let accesorios=0
 class Producto{
     constructor(Nombre,Precio,foto,categoria,Descuento=0,href="#"){
         this.Nombre=Nombre
@@ -24,10 +28,10 @@ let collar=new Producto('Collar',13000,'./Imagenes/13.png','accesorios')
 let gorrito=new Producto('Gorrito',10900,'./Imagenes/14.png','accesorios')
 let pelota=new Producto('Pelota',9600,'./Imagenes/15.png','accesorios')
 let frisby=new Producto('Frisby',12000,'./Imagenes/16.png','accesorios')
-let huesito=new Producto('Huesito',9000,'./Imagenes/17.png','caninos')
-let jaula=new Producto('Jaula',15000,'./Imagenes/18.png','caninos')
-let ratonsito=new Producto('Ratonsito',5000,'./Imagenes/19.png','caninos')
-let huesitoId=new Producto('Hueso de ID',7000,'./Imagenes/20.png','caninos')
+let huesito=new Producto('Huesito',9000,'./Imagenes/17.png','accesorios')
+let jaula=new Producto('Jaula',15000,'./Imagenes/18.png','accesorios')
+let ratonsito=new Producto('Ratonsito',5000,'./Imagenes/19.png','accesorios')
+let huesitoId=new Producto('Hueso de ID',7000,'./Imagenes/20.png','accesorios')
 let affenpinscher=new Producto('Affenpinscher',1200000,'./Imagenes/21.png','caninos')
 let shibaInuJapones=new Producto("Shiba Inu Japonés",1700000,'./Imagenes/22.png','caninos')
 let basenji=new Producto('Basenji',1500000,'./Imagenes/23.png','caninos')
@@ -40,13 +44,50 @@ let beauceron=new Producto('Beauceron',1600000,'./Imagenes/29.png','caninos')
 let bedlingtonTerrier=new Producto('Bedlington Terrier',1300000,'./Imagenes/30.png','caninos')
 const productos=[bergamasco,chihuahua,boxer,beagle,abisinio,bengali,burmes,balines,canario,loro,periquito,cacatua,collar,gorrito,pelota,frisby,huesito,jaula,ratonsito,huesitoId,affenpinscher,shibaInuJapones,basenji,bassetAzuldeGascuña,bassetGrifonVandeanoGrande,bassetGrifonVandeanoPequeño,bassetHound,bassetLeonadoDeBretaña,beauceron,bedlingtonTerrier]
 const main=document.getElementById('venta')
+
+
+function ofertasPerros(descuento) {
+    productos.forEach(Element => {
+        if (Element.categoria=='caninos') {
+            Element.Descuento=descuento    
+        }
+    })
+}
+function ofertasGatos(descuento){
+    productos.forEach(Element => {
+        if(Element.categoria=='felinos'){
+            Element.Descuento=descuento
+        }
+    }) 
+}
+function ofertasAves(descuento) {
+    productos.forEach(Element => {
+        if (Element.categoria=='aves') {
+            Element.Descuento=descuento
+        }
+    });
+}
+function ofertasAccesorios(descuento) {
+    productos.forEach(Element => {
+        if (Element.categoria=='accesorios') {
+            Element.Descuento=descuento
+        }
+    });
+}
+ofertasPerros(10)
+ofertasGatos(15)
+ofertasAves(20)
+ofertasAccesorios(50)
+
+
+
 productos.forEach((producto)=>{
     if (producto.Descuento===0) {
         let inner=`<a href=${producto.href} class="producto" id=${producto.Nombre} ><img src=${producto.foto} alt=${producto.Nombre}>${producto.Nombre}<p><h4>$${producto.Precio}</h4></p></a>`
         main.innerHTML+=inner
     }else{
         let descuento=(100-producto.Descuento)*producto.Precio/100
-        let inner=`<a href=${producto.href} class="producto"><p>${producto.Descuento}% </p><img src=${producto.foto} alt=${producto.Nombre} ><p>${producto.Nombre} </p><p><h5>Precio normal: <span>$${producto.Precio}<span></h5><p><p><h5>Precio ahora: $${descuento} </h5></p></a>`
+        let inner=`<a href=${producto.href} class="producto" id=${producto.Nombre}><p>${producto.Descuento}% </p><img src=${producto.foto} alt=${producto.Nombre} ><p>${producto.Nombre} </p><p><h5>Precio normal: <span>$${producto.Precio}<span></h5><p><p><h5>Precio ahora: $${descuento} </h5></p></a>`
         main.innerHTML+=inner
     }
 })
