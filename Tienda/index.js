8a385c9a0b55a61ac004d5556c942af753f3cbff
juan123sebastian <122684235+juan123sebastian@users.noreply.github.com>
2023-01-17 23:03:12 +0000

    Subo oferta tienda

--- Tienda/index.js
+++ Tienda/index.js
@@ -2,6 +2,7 @@ let caninos=0
 let felinos=0
 let aves=0
 let accesorios=0
+let fechaActual=new Date()
 class Producto{
     constructor(Nombre,Precio,foto,categoria,Descuento=0,href="#"){
         this.Nombre=Nombre
@@ -74,10 +75,62 @@ function ofertasAccesorios(descuento) {
         }
     });
 }
-ofertasPerros(10)
-ofertasGatos(15)
-ofertasAves(20)
-ofertasAccesorios(50)
+
+if(fechaActual.getDate()>=5 && fechaActual.getDate()<=15 && fechaActual.getMonth()==0){ 
+    ofertasGatos(20)
+}
+if (fechaActual.getDate()>=1 && fechaActual.getDate()<=10 && fechaActual.getMonth()==1) {
+    ofertasAccesorios(20)
+}
+if (fechaActual.getDate()>=15 && fechaActual.getDate()<=20 && fechaActual.getMonth()==2) {
+    ofertasAccesorios(30)
+    ofertasAves(30)
+    ofertasPerros(30)
+    ofertasGatos(30)
+}
+if (fechaActual.getDate()>=13 && fechaActual.getDate()<=25 && fechaActual.getMonth()==3) {
+    ofertasGatos(12)
+}
+if (fechaActual.getDate()>=20 && fechaActual.getDate()<=25 && fechaActual.getMonth()==4) {
+    ofertasPerros(20)
+    ofertasGatos(30)
+}
+if (fechaActual.getDate()>=10 && fechaActual.getDate()<=15 && fechaActual.getMonth()==5) {
+    ofertasPerros(13)
+}
+if (fechaActual.getDate()>=12 && fechaActual.getDate()<=20 && fechaActual.getMonth()==6) {
+    ofertasAves(20)
+}
+if (fechaActual.getDate()>=20 && fechaActual.getDate()<=25 && fechaActual.getMonth()==7) {
+    ofertasAccesorios(24)
+    ofertasAves(24)
+    ofertasPerros(24)
+    ofertasGatos(24)
+}
+if (fechaActual.getDate()>=20 && fechaActual.getDate()<=25 && fechaActual.getMonth()==8) {
+    ofertasAccesorios(20)
+}
+if (fechaActual.getDate()>=25 && fechaActual.getDate()<=30 && fechaActual.getMonth()==9) {
+    ofertasGatos(19)
+}
+if (fechaActual.getDate()>=2 && fechaActual.getDate()<=7 && fechaActual.getMonth()==10) {
+    ofertasPerros(25)
+}
+if (fechaActual.getDate()>=15 && fechaActual.getDate()<=25 && fechaActual.getMonth()==11) {
+    ofertasAccesorios(28)
+    ofertasAves(28)
+    ofertasPerros(28)
+    ofertasGatos(28)
+}
+
+if (fechaActual.getDay()==5 && fechaActual.getDate()>=24 && fechaActual.getDate()<=30 && fechaActual.getMonth()==10) {
+    ofertasAccesorios(20)
+    ofertasAves(20)
+    ofertasPerros(20)
+    ofertasGatos(20)
+}
+
+
 
 
 
